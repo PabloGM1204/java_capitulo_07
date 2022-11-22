e52a8b53315859c5a0bf10c230cbaaedac48bc89
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-22 13:13:25 +0100

    Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Ejercicio5{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa lee varios números y te dice cual es el máximo y cual el minimo");
+        
+    }
+}
