bd9cd2dee584eb8c51f52d7a58c44d2d9973499a
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-23 18:05:01 +0100

    Ejercicio5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,6 +3,30 @@ public class Ejercicio5{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Este programa lee varios números y te dice cual es el máximo y cual el minimo");
-        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int [] array = new int[10];
+        System.out.println("Dime 10 números:");
+        for(int i = 0; i<10; i++){
+            int num = sc.nextInt();
+            array[i] = num;
+            if(max<array[i]){
+                max = array[i];
+            }
+            if(min>array[i]){
+                min = array[i];
+            }
+        }
+        for(int i = 0; i<10; i++){
+            if(i == 9){
+                System.out.print(array[i]);
+            }else if(max==array[i]){
+                System.out.print(array[i]+" máximo, ");
+            }else if(min==array[i]){
+                System.out.print(array[i]+" mínimo, ");
+            }else{
+                System.out.print(array[i]+", ");
+            }
+        }
     }
 }
