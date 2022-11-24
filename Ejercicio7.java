7419d490bf2f663473bc0b96d83059827bde1b34
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-24 08:44:41 +0100

    Ejercicio7

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class Ejercicio7{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Este programa genera 100 números aleatorios");
         int [] array = new int [100];
+        String verde = "\033[32m";
+        String blanco = "\033[37m";
         for(int i = 0; i<100; i++){
             array[i] = (int)(Math.random()*21);
             System.out.print(array[i]+" ");
+        }
+        System.out.println("");
+        System.out.print("Introduzca un número de los que se han mostrado: ");
+        int numvisto = sc.nextInt();
+        System.out.print("Introduzca el valor por el que quiere sustituirlo: ");
+        int numcambio = sc.nextInt();
+        for(int i = 0; i<100; i++){
+            if(array[i]==numvisto){
+                array[i] = numcambio;
+                System.out.print(verde+"\""+array[i]+"\" "+blanco);
+            }else{
+                System.out.print(array[i]+" ");
+            }
         }
 
     }
