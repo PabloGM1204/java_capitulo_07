16755e220b755c45aab3c5d29463d7b747860e00
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-27 16:00:24 +0100

    Ejercicio23

---
+++ Ejercicio23.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Ejercicio23 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa pide 20 números y los metes en una array");
+        int[][] nums = new int[4][5];
+        int num;
+        int sumafila = 0;
+        int sumColum = 0;
+        int sumTo = 0;
+        System.out.println("Dime números:");
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j<5; j++){
+                nums[i][j] = num = sc.nextInt();
+            }
+        }
+        System.out.println("");
+        for(int i = 0; i<4; i++){
+            sumafila = 0;
+            for(int j = 0; j<5; j++){
+                System.out.printf("%-15d",nums[i][j]);
+                sumafila += nums[i][j];
+            }
+            sumColum += nums[i][i];
+            System.out.printf("%3d fila %d\n",sumafila,i);
+        }
+        for(int j = 0; j<5; j++){
+            sumColum = 0;
+            for(int i = 0; i<4; i++){
+                sumColum += nums[i][j];
+            }
+            sumTo += sumColum;
+            System.out.printf("%d Columna %-3d ",sumColum,j);
+        }
+        System.out.printf("%3d Total",sumTo);
+    }
+}    
