c1293bf629aa13c7956bc0618a8d501da3c2fbd3
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-27 16:41:45 +0100

    Ejercicio26

---
+++ Ejercicio26.java
@@ -0,0 +1,36 @@
+public class Ejercicio26{
+    public static void main(String[] args) {
+        System.out.println("Este programa rellena un array y te muestra el máximo y el minimo");
+        int[][] nums = new int[6][10];
+        int max = 0;
+        int min = 1001;
+        int posFilmax = 0;
+        int posColumax = 0;
+        int posFilmin = 0;
+        int posColumin = 0;
+        for(int i = 0; i<10; i++){
+            System.out.print("      Columna "+i);
+        }
+        System.out.println("");
+        for(int i = 0; i<6; i++){
+            System.out.print("Fila "+i+"   ");
+            for(int j = 0; j<10; j++){
+                nums[i][j] = (int)(Math.random()*1001);
+                System.out.printf("%-15d",nums[i][j]);
+                if(max<nums[i][j]){
+                    max = nums[i][j];
+                    posFilmax = i;
+                    posColumax = j;
+                }
+                if(min>nums[i][j]){
+                    min = nums[i][j];
+                    posFilmin = i;
+                    posColumin = j;
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("El máximo es: "+max+" y esta en la fila  "+posFilmax+" y Columna "+posColumax);
+        System.out.println("El mínimo es: "+min+" y esta en la fila  "+posFilmin+" y Columna "+posColumin);
+    }
+}
