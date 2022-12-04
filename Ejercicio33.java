4a897bedca1461c1f88de7570c656767ddf471d3
pabloGm1204 <pabloGm1204@gmail.com>
2022-12-04 18:27:35 +0100

    Ejercicio33

--- Ejercicio33.java
+++ Ejercicio33.java
@@ -1,26 +1,30 @@
 public class Ejercicio33{
     public static void main(String[] args) {
-        System.out.println("Este programa calcula la estatura media y máxima");
-        String[] p = {"España", "Rusia", "Japón", "Australia"};
-        int[][] esta = new int[4][10];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 10; j++) {
-                esta[i][j] = (int) (Math.random() * (210 - 140 + 1)) + 140;
+        System.out.println("Este programa muestra la diagonal de un array bidimensional");
+        int[][] n = new int[9][9];
+        int max = 500;
+        int min = 900;
+        int media = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                n[i][j] = (int) (Math.random() * 401) + 500;
+                System.out.printf("%5d", n[i][j]);
             }
+            System.out.println();
         }
-        System.out.printf("%64s\n", "MED MIN MAX");
-        for (int i = 0; i < 4; i++) {
-            int max = 140;
-            int min = 210;
-            int t = 0;
-            System.out.printf("%9s:", p[i]);
-            for (int estatura : esta[i]) {
-                System.out.printf("%4d", estatura);
-                max = estatura > max ? estatura : max;
-                min = estatura < min ? estatura : min;
-                t += estatura;
+        System.out.print("\nDiagonal desde la esquina inferior izquierda ");
+        System.out.println("a la esquina superior derecha: ");
+        for (int i = 0; i < 9; i++) {
+            int num = n[8 - i][i];
+            System.out.print(num + " ");
+            if (num > max) {
+                max = num;
             }
-            System.out.printf(" |%4d%4d%4d\n", t / 10, min, max);
+            if (num < min) {
+                min = num;
+            }
+            media += num;
         }
-    }
+        System.out.print("\nEl máximo: "+max+" El mínimo: "+min+" La media: "+((double)media/10));
+    }    
 }
